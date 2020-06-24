bda42a10dda7759132cada9379430ec0fd753ce1
Jordan Walker <jordanswalker28@gmail.com>
2020-06-25 00:51:03 +0100

    Added a game loop

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class NumberWizard : MonoBehaviour
 {
 
-    int max = 1001;
+    int max = 1000;
     int min = 1;
     int guess = 500;
+    int count = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        RestartVariables();
+
         Debug.Log("Welcome to Number Wizard");
         Debug.Log("Pick a Number for the Wizard");
         Debug.Log($"The Highest Number you can pick is {max}, the Lowest you can pick is {min}");
         Debug.Log("Good Luck!");
         Debug.Log($"Tell me if your number is higher or lower than {guess}");
         Debug.Log("Push Up = Higher, Push Down = Lower");
-        
     }
 
     // Update is called once per frame
@@ -26,24 +33,37 @@ public class NumberWizard : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Lower was pressed");
+            count++;
             min = guess;
-            guess = (max + min) / 2; 
-            Debug.Log(guess);
+            NextGuess();
         }
 
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("Higher was pressed");
+            count++;
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log(guess);
+            NextGuess();
         }
 
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Enter was pressed");
-            Debug.Log($"{guess} is right, You are correct!!!!");
+            Debug.Log($"{guess} is right, I knew it all along!!!!");
+            Debug.Log($"Just ignore the other {count - 1} guesses, I was just keeping you on your toes");
+            StartGame();
         }
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log($"is it higher or lower than {guess}?");
+    }
+
+    void RestartVariables()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+        count = 0;
     }
 }
