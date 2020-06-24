769caa01739d46fc2c6c442b453b742a2761d632
Jordan Walker <jordanswalker28@gmail.com>
2020-06-24 23:48:24 +0100

    Added more debug messages

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -8,6 +8,11 @@ public class NumberWizard : MonoBehaviour
     void Start()
     {
         Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Pick a Number for the Wizard");
+        Debug.Log("The Highest Number you can pick is 1000, the Lowest you can pick is 0");
+        Debug.Log("Good Luck!");
+
+
     }
 
     // Update is called once per frame
